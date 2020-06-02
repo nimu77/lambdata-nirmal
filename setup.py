1a3ef1fe14ca9b176184daf80cbd489abf3c3348
nimu77 <nirmal.thapa77@outlook.com>
2020-06-02 06:58:06 -0700

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-nirmal", # the name that you will install via pip
-    version="1.0",
+    version="1.1",
     author="Nirmal Thapa",
     author_email="nirmal-thapa@lambdastudents.com",
     description="A short description",
