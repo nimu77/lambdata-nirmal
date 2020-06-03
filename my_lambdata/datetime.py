a4c4b1d24aadb4c4061bb70123ccb862b70fcf09
nimu77 <nirmal.thapa77@outlook.com>
2020-06-03 06:06:42 -0700

    introduced new class

---
+++ my_lambdata/datetime.py
@@ -0,0 +1,21 @@
+# my_lambdata/datetime.py
+
+class DateTime():
+    def __init__(self, year, month, date, hour, minute, seconds):
+        self.year = year
+        self.month = month
+        self.date = date
+        self.hour = hour
+        self.minute = minute
+        self.seconds = seconds
+
+    def timeline(self):
+        print("It was", self.month, self.year)
+
+if __name__ == "__main__":
+
+    timeframe = DateTime(2020, "June", 3, 14, 59, 50)
+    timeframe.timeline()
+
+    timeframe2 = DateTime(1989, "May", 16, 4, 14, 19)
+    timeframe2.timeline()
