1ab303291c6176850b503179aabe82189cb6dde2
nimu77 <nirmal.thapa77@outlook.com>
2020-06-03 06:23:44 -0700

    updated version for setup and updated readme with new class and function

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-nirmal",  # the name that you will install via pip
-    version="1.6",
+    version="1.7",
     author="Nirmal Thapa",
     author_email="nirmal-thapa@lambdastudents.com",
     description="A short description",
